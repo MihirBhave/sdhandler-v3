e9c59a2e2a52c4630499b1955db64f4811148df0
migzchi <87005004+Shadow-1223@users.noreply.github.com>
2022-12-17 10:55:02 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import { SDClient } from "./structures/Client";
 export * from "./typings/Button";
+export * from "./typings/Autocomplete"
 export * from "./typings/Command";
 export * from "./typings/Sdhandler";
 export * from "./structures/enums";
 export { Command } from "./structures/Command";
+export { Autocomplete } from "./structures/Autocomplete"
 export { Button } from "./structures/Button";
 export { Event } from "./structures/Event";
+
 
 export { SDClient };
